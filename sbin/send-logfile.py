a08931b1b8ac1f06ff5bbd727227fd11e7c5e7b7
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-23 13:53:22 +0300

    Removal of master/slave language

--- sbin/send-logfile.py
+++ sbin/send-logfile.py
@@ -99,9 +99,9 @@ class ESconnection:
         """
         aa=self.es.index(index='posts', doc_type='blog', id=1, body={
             'author': 'Santa Clause',
-            'blog': 'Slave Based Shippers of the North',
+            'blog': 'Subordinate Based Shippers of the North',
             'title': 'Using Celery for distributing gift dispatch',
-            'topics': ['slave labor', 'elves', 'python',
+            'topics': ['subordinate labor', 'elves', 'python',
                        'celery', 'antigravity reindeer'],
             'awesomeness': 0.2
         })
